ed595a1cdb0261b3b83f3870dc34b4f4e880026f
Mareike Haug <mareike.haug@web.de>
2017-01-25 01:45:32 +0100

    Projekt aufräumen

--- Assets/Scripts/MysteryLockOpener.cs
+++ Assets/Scripts/MysteryLockOpener.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class MysteryLockOpener : MonoBehaviour {
 	public int keyCount;
 	public GameObject pointLight;
-	public GameObject winLight;
+	//public GameObject winLight;
+    public Material whiteSkybox;
 
 	public void OpenDoor () {
 		pointLight.SetActive (true);
-		winLight.SetActive (true);
+		//winLight.SetActive (true);
 		this.GetComponentInParent<Rigidbody> ().isKinematic = false;
 		this.GetComponentInParent<HingeJoint> ().useMotor = true;
-	}
+        RenderSettings.skybox = whiteSkybox;
+    }
 }
